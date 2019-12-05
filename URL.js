9321b44a1d038f2d10581998ddf9a38a6f90d906
CHGovindRao <chgovinda.rao@crest.in>
2019-12-05 11:30:45 +0530

    changes in acction file

--- URL.js
+++ URL.js
@@ -1,5 +1,6 @@
 var URLS = {
-    "loginpage" :  "https://signon.springer.com/login?wt_t=1574161088548&wt_eid=2157321311800205027&service=https%3A%2F%2Fwww.springermedizin.de%2Fcallback%3Fclient_name%3DCasClienthttps%3A%2F%2Fwww.springermedizin.de&locale=de&gtm=GTM-MXV5LX&hotjarId=319962&relogin=true&webtrekkId=642902694028228"
+    "loginpage" :  "https://signon.springer.com/login?wt_t=1575448184450&wt_eid=2156326165200508795&service=https%3A%2F%2Fwww.springermedizin.de%2Fcallback%3Fclient_name%3DCasClienthttps%3A%2F%2Fwww.springermedizin.de&locale=de&gtm=GTM-MXV5LX&hotjarId=319962&relogin=true&webtrekkId=642902694028228&_ga=2.154994619.208282240.1575366261-603564994.1563261653",
+    "CME"   : "https://www.springermedizin.de/cme/6625940"
 };
 
 module.exports = URLS;
--- actions.js
+++ actions.js
@@ -1,13 +1,17 @@
 var actions = {
+    "LoginPage" : [
+       
+        "set field #username to testaccount8-bsmo@bsl.nl",
+        "set field #password to testaccount8-bsmo",
+        "click element input[name='submit']",
+        "wait for url to be https://www.springermedizin.de/myprofile-dashboard",
+    ],
     "CME" : [
-                "set field #username to testaccount8-bsmo@bsl.nl",
-                "set field #password to testaccount8-bsmo",
-                "click element input[name='submit']",
-                "wait for url to be https://www.springermedizin.de/myprofile-dashboard",
-                "navigate to https://www.springermedizin.de/cme/6625940"
-            
+                
+                
+                "click element .main-navigation-teaser>li>a[title ='CME']>span",
+                "wait for url to be https://www.springermedizin.de/cme/6625940"
             ]
-
 };
 
 module.exports = actions;
